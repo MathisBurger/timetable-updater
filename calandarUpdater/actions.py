e76fbabfeaf5e465a666900689349e0a61ce1549
Mathis <handymathis@gmail.com>
2021-10-01 20:51:48 +0200

    [TASK] added code documentation

--- calandarUpdater/actions.py
+++ calandarUpdater/actions.py
@@ -5,11 +5,14 @@ import os
 
 from calandarUpdater.initialize import InitializeCalendar
 
+
 class CalendarActions:
 
     def __init__(self):
         self.service = InitializeCalendar().start()
 
+    # This method fetches all events that happen at the
+    # given time on the calculated day
     def fetch_all_events_at_time(self, startTime, endTime):
         day = self.get_day_to_use()
         startTime = day.isoformat() + "T" + startTime + ":00Z"
@@ -20,6 +23,7 @@ class CalendarActions:
             calendarId=os.environ.get("CALENDAR_ID")
         ).execute().get('items', [])
 
+    # This method updates a lesson with the given id
     def update_lesson(self, summary, room, eventId):
         self.service.events().patch(
             calendarId=os.environ.get("CALENDAR_ID"),
@@ -31,7 +35,8 @@ class CalendarActions:
             sendUpdates="all"
         ).execute()
 
-
+    # This method checks what day is
+    # the relevant day in the substitution plan
     def get_day_to_use(self):
         now = datetime.now()
         if now.hour > 16:
